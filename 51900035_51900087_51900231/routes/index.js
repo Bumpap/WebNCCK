fb6ba9db7d1f82d1d8f88b7f60cac99c2bd9bff1
Gvdien <giangvinhdien.tdtu@gmail.com>
2021-12-12 11:02:31 +0700

    Thêm bootstrap css cho layout index, login

--- 51900035_51900087_51900231/routes/index.js
+++ 51900035_51900087_51900231/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login',function(req,res,next){
+  res.render('login');
 });
 
 module.exports = router;
