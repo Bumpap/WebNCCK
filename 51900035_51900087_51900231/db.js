e9878c59f4b5c150e16c804d3daa774d826cef2f
Gvdien <giangvinhdien.tdtu@gmail.com>
2021-12-13 09:46:11 +0700

    Tạo db

---
+++ 51900035_51900087_51900231/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const connectionString = "mongodb://localhost:27017/51900035_51900087_51900231"
+if (!connectionString){
+    console.error('MongoDB connection string missing!')
+    process.exit(1)
+}
+
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', err => {
+    console.error('MongooseDB error: ' +err.message)
+})
+
+db.once('open', () => console.log('MongoDB connection established'))
