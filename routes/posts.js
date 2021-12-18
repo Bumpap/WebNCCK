a8a620713d9780f87c4429d401cebdff9d691546
Gvdien <90970661+Gvdien@users.noreply.github.com>
2021-12-18 11:47:08 +0700

    Delete routes directory

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var postsController = require('../controllers/PostsController')
-
-router.post('/create',postsController.create);
-
-router.get('/list',postsController.list);
-
-module.exports = router;
