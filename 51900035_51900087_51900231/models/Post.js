d7057ceda0d813d22ac33cb63a311165b2e2b5f9
Gvdien <giangvinhdien.tdtu@gmail.com>
2021-12-18 11:35:06 +0700

    foler

---
+++ 51900035_51900087_51900231/models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const postSchema = mongoose.Schema({
+    content: String,
+    creator: String,
+    created_at: Date,
+    updated_at: Date
+})
+
+const Post = mongoose.model('Post', postSchema)
+module.exports = Post
