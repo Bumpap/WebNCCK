5bf36b3537a697136b170775f8875dc8a1fb188b
Gvdien <giangvinhdien.tdtu@gmail.com>
2022-01-05 19:53:49 +0700

    ajax.js => main.js

---
+++ 51900035_51900087_51900231/public/javascripts/main.js
@@ -0,0 +1,141 @@
+$(document).ready(function () {
+    $("#btn_Submit").on("submit", function (event) {
+        event.preventDefault();
+        let khoa = $("khoa").val();
+        let lop = $("#lop").val();
+        let name = $("#fullname").val();
+        let email = $("#eMail").val();
+        let avatar = $('#user-avatar').attr('src');
+        $.ajax({
+            url: "/profile",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ khoa: khoa, lop: lop, name: name, email: email, avatar: avatar }),
+            success: function (res) {
+                $('#khoa').val() = khoa;
+                $("#lop").val() = lop;
+                $("#fullname").val() = name;
+                $("#eMail").val() = email;
+                $('#user-avatar').attr('src') = avatar;
+            }
+
+        })
+    })
+
+    $("#btn_changePWD").on("submit", function (event) {
+        event.preventDefault();
+        let newPWD = $("newPWD").val();
+
+        $.ajax({
+            url: "/changePWD",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ password: newPWD }),
+            success: function (res) {
+                $("newPWD").val() = newPWD;
+            }
+
+        })
+    })
+
+    $("body").on("click", "#delete", (event) => {
+        event.preventDefault();
+        let id = $(event.target).data("id");
+
+        $.ajax({
+            url: "/deletePostBtn",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ id: id }),
+            success: function (res) {
+                console.log(res);
+                let div = document.getElementById(res);
+                console.log(div);
+                div.remove();
+            }
+
+        })
+    })
+
+
+    $("body").on("click", "#deleteNotifi", (event) => {
+        event.preventDefault();
+        let id = $(event.target).data("id");
+
+        $.ajax({
+            url: "/deleteNotifi",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ id: id }),
+            success: function (res) {
+                console.log(res);
+                let div = document.getElementById(res);
+                console.log(div);
+                div.remove();
+            }
+
+        })
+    })
+
+    $("body").on("click", "#edit", (event) => {
+        event.preventDefault();
+        let id = $(event.target).data("id");
+        let savePost = document.getElementById("editBtn");
+        savePost.setAttribute('data-id', id);
+
+        $.ajax({
+            url: "/editPostBtn",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ id: id }),
+            success: function (res) {
+                let a = document.getElementById(res);
+                let b = a.querySelector("#" + "user-status").innerHTML;
+                document.getElementById("user-status-edit").innerHTML = b;
+                console.log(b);
+                console.log(id);
+            }
+        })
+    })
+
+    $("body").on("click", "#editBtn", (event) => {
+        event.preventDefault();
+        let id = $(event.target).data("id");
+        let user_edit = document.getElementById("user-status-edit").value;
+        $.ajax({
+            url: "/saveEdit",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ id: id, content: user_edit }),
+            success: function (res) {
+                console.log(res);
+            }
+
+        })
+    })
+
+
+    $("body").on("click", "#myProfile", (event) => {
+        // event.preventDefault();
+        let id = $(event.target).data("id");
+        console.log(id)
+        let content = document.getElementById("display_name").innerHTML;
+        console.log(content)
+        $.ajax({
+            url: "/myProfile",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ content: content }),
+            success: function (res) {
+                console.log(res);
+            }
+
+        })
+    })
+
+    // document.getElementById("#myProfile").onclick = function () {
+    //     let content = document.getElementById("display_name").innerHTML;
+    //     console.log(content)
+    // }
+
+})
